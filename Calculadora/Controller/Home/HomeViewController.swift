d3e43de4fada9d1b3a4bd84680c8b939cda4beb8
Alvaro Cantarino <alvaro.cj05@gmail.com>
2020-10-26 10:26:15 +0100

    Adding operations logic

--- Calculadora/Controller/Home/HomeViewController.swift
+++ Calculadora/Controller/Home/HomeViewController.swift
@@ -38,6 +38,49 @@ final class HomeViewController: UIViewController {
     @IBOutlet weak var operatorMultiplication: UIButton!
     @IBOutlet weak var operatorDivision: UIButton!
     
+    // MARK: - Variables
+    
+    private var total:Double = 0        // Total
+    private var temp:Double = 0         // Valor por pantalla
+    private var operating = false       // Indicar si se ha seleccionado un operador
+    private var decimal = false         // Indicar si el valor es decimal
+    private var operation: OperatorType = .none // Operacion actual
+    
+    // MARK: - Constants
+    
+    private let kDecimalSeparator = Locale.current.decimalSeparator!
+    private let kMaxLength = 9
+    private let kMaxValue: Double = 999999999
+    private let kMinValue: Double = 0.00000001
+    
+    private enum OperatorType {
+        case none, addiction, substraction, multiplication, division, percent
+    }
+    
+    // Formateo de valores auxiliar
+    private let auxFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        let locale = Locale.current
+        formatter.groupingSeparator = ""
+        formatter.decimalSeparator = locale.decimalSeparator
+        formatter.numberStyle = .decimal
+        return formatter
+        
+    }()
+    
+    // Formateo para valores por defecto
+    private let printFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        let locale = Locale.current
+        formatter.groupingSeparator = locale.groupingSeparator
+        formatter.decimalSeparator = locale.decimalSeparator
+        formatter.numberStyle = .decimal
+        formatter.maximumIntegerDigits = 9
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 8
+        return formatter
+        
+    }()
     
     // Mark: - Initialization
     
@@ -76,42 +119,146 @@ final class HomeViewController: UIViewController {
         operatorMultiplication.round()
         operatorDivision.round()
         
+        numberDecimal.setTitle(kDecimalSeparator, for: .normal)
+        
+        result()
+        
     }
     
     // Mark: - Button Actions
     @IBAction func operatorPlusMinusAction(_ sender: UIButton) {
+        temp = temp * (-1)
+        resultLabel.text = printFormatter.string(from: NSNumber(value: temp))
         sender.bounce()
     }
     @IBAction func operatorPercentAction(_ sender: UIButton) {
+        
+        if operation != .percent {
+            result()
+        }
+        operating = true
+        operation = .percent
+        result()
         sender.bounce()
     }
     @IBAction func operatorResultAction(_ sender: UIButton) {
+        result()
         sender.bounce()
     }
     @IBAction func operatorAdditionAction(_ sender: UIButton) {
+        result()
+        operating = true
+        operation = .addiction
         sender.bounce()
     }
     @IBAction func operatorSubstractionAction(_ sender: UIButton) {
+        result()
+        operating = true
+        operation = .substraction
         sender.bounce()
     }
     @IBAction func operatorMultiplicationAction(_ sender: UIButton) {
+        result()
+        operating = true
+        operation = .multiplication
         sender.bounce()
     }
     @IBAction func operatorDivisionAction(_ sender: UIButton) {
+        result()
+        operating = true
+        operation = .division
         sender.bounce()
     }
     @IBAction func operatorACAction(_ sender: UIButton) {
+        clear()
         sender.bounce()
     }
     
     @IBAction func numberDecimalAction(_ sender: UIButton) {
+        
+        let currentTemp = auxFormatter.string(from: NSNumber(value: temp))!
+        if !operating && currentTemp.count >= kMaxLength {
+            return
+        }
+        resultLabel.text = resultLabel.text! + kDecimalSeparator
+        decimal = true
+        
         sender.bounce()
     }
     
     @IBAction func numberAction(_ sender: UIButton) {
-        sender.bounce()
-        print(sender.tag)
         
+        
+        operatorAC.setTitle("C", for: .normal)
+        var currentTemp = auxFormatter.string(from: NSNumber(value: temp))!
+        if !operating && currentTemp.count >= kMaxLength {
+            return
+        }
+        
+        // Hemos seleccionado una operacion
+        if operating {
+            total = total == 0 ? temp : total
+            resultLabel.text = ""
+            currentTemp = ""
+            operating = false
+        }
+    
+        // Hemos seleccionado decimales
+        if decimal {
+            currentTemp = "\(currentTemp)\(kDecimalSeparator)"
+            decimal = false
+        }
+        
+        let number = sender.tag
+        temp = Double(currentTemp + String(number))!
+        resultLabel.text = printFormatter.string(from: NSNumber(value: temp))
+        sender.bounce()
     }
     
+    // Limpiar los valores
+    private func clear() {
+        operation = .none
+        operatorAC.setTitle("AC", for: .normal)
+        if temp != 0 {
+            temp = 0
+            resultLabel.text = "0"
+        } else {
+            total = 0
+            result()
+        }
+    }
+    
+    // Obtiene el resultado final
+    private func result() {
+        
+        switch operation {
+            
+        case .none:
+            // No hace nada
+            break
+        case .addiction:
+            total = total + temp
+            break
+        case .substraction:
+            total = total - temp
+            break
+        case .multiplication:
+            total = total * temp
+            break
+        case .division:
+            total = total / temp
+            break
+        case .percent:
+            temp = temp / 100
+            total = temp
+            break
+        }
+        
+        // Formateo en pantalla
+        if total <= kMaxValue || total >= kMinValue {
+            resultLabel.text = printFormatter.string(from: NSNumber(value: total))
+        }
+        
+        print("Total: \(total)")
+    }
 }
